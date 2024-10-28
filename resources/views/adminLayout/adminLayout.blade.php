3590a466bbf7a801bd6e193fc5a1c308f773e0be
mhmmdazh <c030322053@mahasiswa.poliban.ac.id>
2024-10-28 08:54:24 +0800

    admin

--- resources/views/adminLayout/adminLayout.blade.php
+++ resources/views/adminLayout/adminLayout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{$title}}</title>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@9.0.3"></script>
 </head>
 
 <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -65,5 +65,10 @@ export default {
             },
         },
     },
-    plugins: [require("flowbite/plugin")],
+    plugins: [
+        require('flowbite/plugin')({
+            datatables: true,
+        }),
+        // ... other plugins
+    ],
 };
