9be942861b9817de705f48a9ebaac608a01ca545
Abu Husein <huseinabu01@gmail.com>
2024-10-30 10:34:40 +0800

    add edit profile

--- app/Http/Controllers/PeminjamController.php
+++ app/Http/Controllers/PeminjamController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Peminjam;
+use Illuminate\Support\Facades\Storage;
 
 class PeminjamController extends Controller
 {
@@ -20,7 +22,27 @@ class PeminjamController extends Controller
         return view('userPeminjaman',['title'=>'Form Peminjaman']);
     }
 
-    public function editPeminjam(Request $request){
-        dd($request);
+    public function update(Request $request, Peminjam $peminjam){
+        $validateData = $request->validate([
+            'nama_lengkap' => 'required|max:255',
+            'alamat' => 'required',
+            'no_telp' => 'required',
+            'email' => 'required|email',
+            'ktp' => 'nullable'
+        ]);
+
+
+        if($request->file('ktp')){
+            if($peminjam->ktp){
+                Storage::disk('public')->delete($peminjam->ktp);
+            }
+            $validateData['ktp'] = $request->file('ktp')->store('ktp', 'public');
+        }
+
+        Peminjam::where('id', $peminjam->id)->update($validateData);
+        
+        // Peminjam::create($validateData);
+
+        return redirect('/user/profile')->with('success', 'Data Disimpan');
     }
 }
--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -1,38 +1,42 @@
 @extends('userLayout.userLayout')
 
 @section('peminjamLayout')
+    <form action="/user/{{ Auth::user()->id }}/updateProfile" method="POST" enctype="multipart/form-data">
+        @method('put')
+        @csrf
+        <p
+            class=" inset-x-0 top-0 p-4 mb-4 text-sm text-white rounded-lg bg-red-600 dark:bg-gray-800 dark:text-red-400 {{ Auth::user()->is_verified ? 'text-green-500' : 'text-red-500' }}">
+            {{ Auth::user()->is_verified ? 'Data sudah diverifikasi' : 'Data belum diverifikasi' }}
+        </p>
 
-<form>
-    <p class=" inset-x-0 top-0 p-4 mb-4 text-sm text-white rounded-lg bg-red-600 dark:bg-gray-800 dark:text-red-400 {{ Auth::user()->is_verified ? 'text-green-500' : 'text-red-500' }}">
-        {{ Auth::user()->is_verified ? 'Data sudah diverifikasi' : 'Data belum diverifikasi' }}
-    </p>
+        <div class="grid gap-6 mb-6 md:grid-cols-1">
+            <div>
+                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                <input type="text" id="nama" name="nama_lengkap"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value="{{ Auth::user()->nama_lengkap }}" placeholder="John" required />
+                <!-- Status verifikasi -->
 
-    <div class="grid gap-6 mb-6 md:grid-cols-1">
-        <div>
-            <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
-            <input type="text" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{Auth::user()->nama_lengkap}}" placeholder="John" required />
-            <!-- Status verifikasi -->
-
-        </div>
+            </div>
 
             <div>
                 <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat</label>
-                <input type="text" id="alamat"
+                <input type="text" id="alamat" name="alamat"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     value="{{ Auth::user()->alamat }}" placeholder="" required />
             </div>
             <div>
                 <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor
                     Telepon/Handphone</label>
-                <input type="tel" id="phone"
+                <input type="text" id="phone" name="no_telp"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value="{{ Auth::user()->no_telp }}" placeholder="" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+                    value="{{ Auth::user()->no_telp }}" placeholder="" required />
             </div>
 
         </div>
         <div class="mb-6">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
-            <input type="email" id="email"
+            <input type="email" id="email" name="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 value="{{ Auth::user()->email }}" placeholder="john.doe@company.com" required />
         </div>
@@ -41,52 +45,50 @@
         <!-- Password -->
 
         <!-- <div class="mb-6">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
-        </div>
-        <div class="mb-6">
-            <label for="confirm_password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-            <input type="password" id="confirm_password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
-        </div> -->
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+                        </div>
+                        <div class="mb-6">
+                            <label for="confirm_password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                            <input type="password" id="confirm_password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+                        </div> -->
 
         <!-- file input -->
 
-    <div class="flex items-center justify-center w-full my-4">
-        <!-- <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                </svg>
-                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-            </div> -->
-        <input class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" onchange="previewFile(event)">
-    </div>
-    <div id="preview_container" class="flex w-full items-center justify-center w-full my-4 hidden">
-        <img id="preview_image" class="max-w-xs rounded-lg" alt="Image preview" />
-    </div>
-
+        <div class="flex items-center justify-center w-full my-4">
+            <input
+                class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                id="file_input" type="file" onchange="previewFile(event)" name="ktp">
+        </div>
+        
+        <div class="flex w-full items-center justify-center my-4" id="ktp_display">
+            <img 
+                id="preview_image" 
+                class="max-w-xs rounded-lg"
+                alt="Image preview" 
+                src="{{ Auth::user()->ktp ? asset('storage/' . Auth::user()->ktp) : '' }}" 
+                style="{{ Auth::user()->ktp ? '' : 'display: none;' }}" />
+        </div>
+        
         <button type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-        <!-- <a href="#editProfile" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</a> -->
-
-</form>
-<script>
-    function previewFile(event) {
-        const previewContainer = document.getElementById('preview_container');
-        const previewImage = document.getElementById('preview_image');
-        const file = event.target.files[0];
-
-        if (file && file.type.startsWith('image/')) {
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                previewImage.src = e.target.result;
-                previewContainer.classList.remove('hidden'); // Show preview container
-            };
-            reader.readAsDataURL(file);
-        } else {
-            previewContainer.classList.add('hidden'); // Hide preview if not an image
-            previewImage.src = ''; // Clear the image source
-        }
-    }
-</script>
-@endsection
+        
+        <script>
+            function previewFile(event) {
+                const previewImage = document.getElementById('preview_image');
+                const file = event.target.files[0];
+        
+                if (file) {
+                    const reader = new FileReader();
+                    reader.onload = function(e) {
+                        previewImage.src = e.target.result;
+                        previewImage.style.display = 'block'; // Show preview image
+                    };
+                    reader.readAsDataURL(file);
+                } else {
+                    previewImage.style.display = 'none'; // Hide preview if not an image
+                    previewImage.src = ''; // Clear the image source
+                }
+            }
+        </script>        
+    @endsection
