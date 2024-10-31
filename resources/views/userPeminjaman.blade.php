60ffcf3a4bbacf90985d7be6f7019c0b18d0e4f7
Maulidin <mmaulidinabdi@mail.com>
2024-10-31 08:47:55 +0800

    daskl

--- resources/views/userPeminjaman.blade.php
+++ resources/views/userPeminjaman.blade.php
@@ -28,7 +28,7 @@
                 </div>
                 <div class="w-full">
                     <label for="arsip" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Arsip</label>
-                    <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required />
+                    <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                 </div>
                 <div class="w-full">
                     <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Arsip</label>
@@ -40,7 +40,14 @@
                         <option value="PH">Phones</option>
                     </select>
                 </div>
-                
+
+                <div class="sm:col-span-2">
+                    <label for="dokumen_pendukung" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dokumen Pendukung</label>
+                    <input
+                        class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        id="file_input" type="file" onchange="previewFile(event)" name="dokumen_pendukung">
+                </div>
+
                 <div class="sm:col-span-2">
                     <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tujuan Peminjaman</label>
                     <textarea id="description" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
