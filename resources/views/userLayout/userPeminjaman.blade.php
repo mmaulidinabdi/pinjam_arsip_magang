778ca241c4e4497bf9d7108ea948f37f25a8855c
Abu Husein <huseinabu01@gmail.com>
2024-11-13 21:43:58 +0800

    add notif success peminjaman

--- resources/views/userLayout/userPeminjaman.blade.php
+++ resources/views/userLayout/userPeminjaman.blade.php
@@ -2,6 +2,13 @@
 
 @section('peminjamLayout')
     <section class="bg-white dark:bg-gray-900">
+        @if (session()->has('success'))
+            <div id="alert"
+                class="top-0 w-full  mt-4 p-4 mb-4 text-sm text-white rounded-lg bg-green-500 dark:bg-gray-800 dark:text-red-400"
+                role="alert">
+                <span class="font-medium">{{ session('success') }}</span>
+            </div>
+        @endif
         <div class=" mx-auto">
             <form action="/user/peminjaman" method="POST" enctype="multipart/form-data">
                 @csrf
@@ -10,9 +17,9 @@
                 </div>
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                     <!-- <div class="sm:col-span-2">
-                                      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
-                                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
-                                  </div> -->
+                                          <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
+                                          <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                                      </div> -->
                     <input type="hidden" name="peminjam_id" value="{{ Auth::user()->id }}">
                     <div class="w-full">
                         <label for="nama"
